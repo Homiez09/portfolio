f741e4e39d2bb77fc7dcbb10e9a131f272ed4705
HomieZ09 <phumsoen123@gmail.com>
2025-01-27 21:39:21 +0700

    Update Navbar to show loading spinner when resume is unavailable; enhance CardList to handle fetch errors and display appropriate messages

--- src/components/project/CardList.tsx
+++ src/components/project/CardList.tsx
@@ -7,19 +7,22 @@ import { Project } from '@/types/TypeProject';
 import axios from 'axios';
 
 export const CardList: FC<CardListProps> = ({ query }) => {
-    const [projects, setProjects] = useState<Project[]>([]);
+    const [projects, setProjects] = useState<Project[] | null>(null);
+    const [error, setError] = useState<boolean>(false);
 
     const fetchProjects = async () => {
-        await axios.post(`/api/contents`).then((res) => { setProjects(res.data.data) });
+        await axios.post(`/api/contents`).then((res) => {setError(false);setProjects(res.data.data)}).catch((err) => setError(true));
     }
 
     useEffect(() => {
         fetchProjects();
-    }, []);
+        console.log(projects);
+    }, [error]);
 
     if (!projects) {
         return <p>Loading...</p>;
     }
+    if (error) { return <p>Failed to fetch projects,  Please try again.</p>; }
     return (
         <>
             <div className="flex flex-col gap-5 justify-center w-full">
@@ -36,16 +39,4 @@ export const CardList: FC<CardListProps> = ({ query }) => {
             </div>
         </>
     );
-}
-
-export async function getStaticProps() {
-    const res = await fetch('https://.../api/contents');
-    const data = await res.json();
-
-    return {
-        props: {
-            projects: data.data,
-        },
-        revalidate: 60, // Revalidate every 60 seconds
-    };
 }
