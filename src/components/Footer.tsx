47aec9fbeffd8ae9639b2ed4641645c06224d1bb
HomieZ09 <phumsoen123@gmail.com>
2025-02-20 00:21:03 +0700

    Update Footer component to dynamically display version from package.json

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import packageJson from "../../package.json"
 export default function Footer() {
     return (
         <footer className="flex flex-row items-center justify-center w-full h-12 border-t mt-14 text-sm">
-            <Link href="https://github.com/Homiez09">© 2024 | Made with <span className="text-red-500">♥</span> by <strong>Homiez09</strong> <span className="font-bold text-[#0070f3]">v2.11.0</span></Link>
+            <Link href="https://github.com/Homiez09">© 2024 | Made with <span className="text-red-500">♥</span> by <strong>Homiez09</strong> <span className="font-bold text-[#0070f3]">v{packageJson.version}</span></Link>
             
         </footer>
     )
