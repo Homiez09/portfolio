e3782645484e3aaa2582204e24dc278779ba14cd
HomieZ09 <phumsoen123@gmail.com>
2025-02-22 23:20:47 +0700

    Add Healthz component and adjust layout styles for improved spacing

---
+++ src/app/healthz/page.tsx
@@ -0,0 +1,9 @@
+const Healthz = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen container mx-auto absolute top-0 z-[-5]">
+            <h1>Health: 1000/1000</h1>
+        </div>
+    )
+}
+
+export default Healthz;
