d15a17ef5c8eb5786b6e0a967cd33e308cf6b979
HomieZ09 <phumsoen123@gmail.com>
2025-02-22 14:58:37 +0700

    Remove Suspense wrapper from Layout component

--- src/app/project/layout.tsx
+++ src/app/project/layout.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from "react";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <div className={`mt-8`}>
-                <Suspense>
-                    {children}
-                </Suspense>
+                {children}
             </div>
         </>
     );
